f14778a3a9f66fdeb1d6749af8e79fb64a9c267f
JesusMayuri7 <jesus.mayuri.cuellar@gmail.com>
2018-07-16 22:58:04 -0500

    Angular 6 - Prod - view index.php

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,14 +1,13 @@
-<!doctype html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>Area de Remuneraciones</title>
+  <title>Area de Programación</title>
   <base href="/">
 
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
-</head>
+<link rel="stylesheet" href="styles.2637f4b5ec5edca9cee6.css"></head>
 <body>
   <zd-shell>Cargando...</zd-shell>
-<script type="text/javascript" src="inline.bundle.js"></script><script type="text/javascript" src="polyfills.bundle.js"></script><script type="text/javascript" src="styles.bundle.js"></script><script type="text/javascript" src="vendor.bundle.js"></script><script type="text/javascript" src="main.bundle.js"></script></body>
+<script type="text/javascript" src="runtime.d5f9e438a51f541a760f.js"></script><script type="text/javascript" src="polyfills.ab6d321385a732733578.js"></script><script type="text/javascript" src="main.bc39ade09de704d406c6.js"></script></body>
 </html>
